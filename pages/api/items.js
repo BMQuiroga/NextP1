3f10f6fa495ac3ea289f4af246a524f638449cd8
Bruno Quiroga <bquiroga@fi.uba.ar>
2023-03-04 03:56:21 -0300

    uncringening

--- pages/api/items.js
+++ pages/api/items.js
@@ -6,44 +6,44 @@ export default function handler(req, res) {
   res.status(200).json([
     {
     id: 0,
-    title: 'Meguca',
+    title: 'articulo1',
     price: 100,
-    image: 'http://localhost:3000/img/madoka-plush.jpg',
+    image: 'http://localhost:3000/img/art1.png',
     description: 'Lopem Ipsum'
     },
     {
     id: 1,
-    title: 'Homura',
+    title: 'articulo2',
     price: 200,
-    image: 'http://localhost:3000/img/testimonio-homura-og.jpg',
+    image: 'http://localhost:3000/img/art2.png',
     description: 'Lopem Ipsum'
     },
     {
     id: 2,
-    title: 'QB',
+    title: 'articulo3',
     price: 300,
-    image: 'http://localhost:3000/img/kyuubey-plush.jpg',
+    image: 'http://localhost:3000/img/art3.png',
     description: 'Lopem Ipsum'
     },
     {
     id: 3,
-    title: 'Mami',
+    title: 'articulo4',
     price: 1300,
-    image: 'http://localhost:3000/img/mami-plush.png',
+    image: 'http://localhost:3000/img/art4.png',
     description: 'Lopem Ipsum'
     },
     {
     id: 4,
-    title: 'kyoko',
+    title: 'articulo5',
     price: 200,
-    image: 'http://localhost:3000/img/kyoko-plush.jpg',
+    image: 'http://localhost:3000/img/art5.png',
     description: 'Lopem Ipsum'
     },
     {
     id: 5,
-    title: 'Sayaka',
+    title: 'articulo6',
     price: 50,
-    image: 'http://localhost:3000/img/sayaka-plush.png',
+    image: 'http://localhost:3000/img/art6.jpg',
     description: 'Lopem Ipsum'
     },
     
